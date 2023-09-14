3d9b445c1acf81f14b09578ccb29e22d4292cbe7
Maulik A <maulik.ankolia2@gmail.com>
2023-09-14 15:09:02 +0100

    Update etl_dag.py

--- etl_dag.py
+++ etl_dag.py
@@ -8,7 +8,7 @@ default_args = {
     'depends_on_past': False,
     'start_date': datetime(2023, 8, 12),
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retry_delay': timedelta(minutes=1),
 }
 
 dag = DAG('transform_redshift_dag', default_args=default_args, schedule_interval="@once",catchup=False)
@@ -17,9 +17,9 @@ dag = DAG('transform_redshift_dag', default_args=default_args, schedule_interval
 # Define the Glue Job
 transform_task = GlueJobOperator(
     task_id='transform_task',
-    job_name='glue_transform_task',
-    script_location='https://greenbucketdata.s3.eu-west-2.amazonaws.com/python+scripts/transform.py',
-    s3_bucket='https://greenbucketdata.s3.eu-west-2.amazonaws.com/glue/',  # S3 bucket where logs and local etl script will be uploaded
+    job_name='etl',
+    script_location='s3://aws-glue-assets-703537846147-eu-west-2/scripts/energy_etl_job.py',
+    s3_bucket='s3://aws-glue-assets-703537846147-eu-west-2/scripts/',  # S3 bucket where logs and local etl script will be uploaded
     aws_conn_id='aws_connection',  # Set up an AWS connection in Airflow
     region_name="eu-west-2",
     iam_role_name='dataPipeline-RedshiftIamRole-1CRXYWRMCF5KC',
